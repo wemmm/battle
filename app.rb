37ce82749c7731947ef50221d6f9d70da648ad4f
Alex <alex.satur89@gmail.com>
2017-05-24 15:42:56 +0100

    Allows users to add two names and see them on screen

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
   end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+
+  end
+
+  # post '/battle-names' do
+  #     "Battle time!"
+  #   # p params
+  #   # @name = params[:name]
+  #
+  # end
+  #
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,10 @@
-feature 'Testing Infrastructure' do
-  scenario 'can run app and check page content' do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
+
+ feature "Testing user enter names" do
+   scenario "Allows users to submit names and see them on a screen" do
+     visit '/'
+     fill_in :player_1_name, with: "Jennifer"
+     fill_in :player_2_name, with: "Canard"
+     click_button 'Submit'
+     expect(page).to have_content "Jennifer vs. Canard"
+   end
+ end
