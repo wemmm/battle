34df315e66537448ac5b8afd491a3f52cba7b826
wemmm <jenny.wem@gmail.com>
2017-05-24 17:24:49 +0100

    Added failing test for showing hitpoints

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,3 +8,16 @@
      expect(page).to have_content "Jennifer vs. Canard"
    end
  end
+
+ feature "Hitpoint display" do
+   scenario "User can submit HP points and see them" do
+     visit '/'
+     fill_in :player_1_name, with: "Jennifer"
+     fill_in :player_1_HP, with: 50
+     fill_in :player_2_name, with: "Canard"
+     fill_in :player_2_HP, with: 50
+     click_button 'Submit'
+     expect(page).to have_content "Opponent has 50HP!"
+
+   end
+ end
