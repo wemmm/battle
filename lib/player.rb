5489e658bd984ada15c21ed3a72387cd8f3b8ba1
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-25 16:10:56 +0100

    Reworked player spec & player class -Specs need some adjustment...

--- lib/player.rb
+++ lib/player.rb
@@ -2,12 +2,7 @@ require 'app'
 
 class Player
 
-  def initialize(battle = Battle.new)
-    @battle = battle
-  end
-
-  def gives_name
-    @battle.player_1_name
+  def initialize
   end
 
 end
--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -1,12 +1,13 @@
 require 'player'
-require 'app'
 
 describe Player do
-  subject(:name) { described_class.new }
-  it 'returns its name' do
-    name_double = double(:battle)
-    allow(name_double).to receive(:player_1_name).and_return("cat")
-    name1 = Player.new(name_double)
-    expect(name.gives_name).to eq "cat"
+  subject(:dog) { described_class.new ("Dog")}
+
+  describe '#name' do
+    it 'returns its name' do
+    # name_double = double(:battle)
+    # allow(name_double).to receive(:player_1_name).and_return("cat")
+    # name1 = Player.new(name_double)
+    expect(dog.name).to eq "Dog"
   end
 end
