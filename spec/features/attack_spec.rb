d5132eb6fe3ae896d2a0374c5473d75347e887e0
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-25 14:39:23 +0100

    Added functionality to attack button & gives user attack message

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,7 +2,7 @@
 feature "allows player 1 to attack player 2" do
   scenario "player 1 can attack player 2" do
     sign_in_and_play
-    click_button('attack')
-    expect(page).to have_content("Opponent has 40HP")
+    click_button('Attack!')
+    expect(page).to have_content("Jennifer attacked Canard")
   end
 end
