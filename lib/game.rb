0a1afe91b80ecd206f9112b7380da8e300b1da2b
Kavita <kkavita92@gmail.com>
2017-05-26 13:52:13 +0100

    Errors reduced from galore to nil

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,7 @@ class Game
 
   attr_reader :player1, :player2
 
-  def intialize(player1 = Player.new, player2 = Player.new)
+  def initialize(player1 = Player.new, player2 = Player.new)
     @player1 = player1
     @player2 = player2
   end
